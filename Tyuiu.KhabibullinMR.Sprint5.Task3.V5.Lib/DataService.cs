7256b6830e50cc22a024ee4188b0f3946b4df7b7
Marat Khabibullin <140619138+HabibullinMarat@users.noreply.github.com>
2024-11-29 23:05:22 +0500

    Task3

---
+++ Tyuiu.KhabibullinMR.Sprint5.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KhabibullinMR.Sprint5.Task3.V5.Lib
+{
+    public class DataService : ISprint5Task3V5
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}OutPutFileTask3.bin";
+
+            double y = Math.Round(2 * Math.Pow(x, 3) + (1 / 2) * Math.Pow(x, 2) - 3.5 * x + 2, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), System.Text.Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(x));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KhabibullinMR.Sprint5.Task3.V5/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KhabibullinMR.Sprint5.Task3.V5.Lib;
+
+namespace Tyuiu.KhabibullinMR.Sprint5.Task3.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            Console.WriteLine("Значение Х: " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                 ");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
