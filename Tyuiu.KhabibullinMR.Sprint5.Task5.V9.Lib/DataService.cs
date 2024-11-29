1f535a1696a197af291c64f644e2f48b3fac221b
Marat Khabibullin <140619138+HabibullinMarat@users.noreply.github.com>
2024-11-30 00:47:01 +0500

    Task5

---
+++ Tyuiu.KhabibullinMR.Sprint5.Task5.V9.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KhabibullinMR.Sprint5.Task5.V9.Lib
+{
+    public class DataService : ISprint5Task5V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            string[] s = strX.Split(',');
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = s[i].Replace(".", ",");
+            }
+
+            double[] dS = new double[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                dS[i] = Convert.ToDouble(s[i]);
+            }
+            double max = dS.Max();
+
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.KhabibullinMR.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KhabibullinMR.Sprint5.Task5.V9.Lib;
+
+namespace Tyuiu.KhabibullinMR.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+    }
+}
