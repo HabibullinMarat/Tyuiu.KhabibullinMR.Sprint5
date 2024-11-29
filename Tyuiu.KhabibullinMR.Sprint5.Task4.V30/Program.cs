e87280bbbe79c3cb31d177db83529ce80860590a
Marat Khabibullin <140619138+HabibullinMarat@users.noreply.github.com>
2024-11-30 00:00:54 +0500

    Task4

---
+++ Tyuiu.KhabibullinMR.Sprint5.Task4.V30/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KhabibullinMR.Sprint5.Task4.V30.Lib;
+
+namespace Tyuiu.KhabibullinMR.Sprint5.Task4.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Path.GetTempPath()}InPutDataFileTask4V30.txt";
+
+            Console.WriteLine("Данные находятся в файлй: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                 ");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
